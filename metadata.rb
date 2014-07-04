665f2d78a591c7a793651e681ad11635aa224de4
Steve Purr <spurr@articulate.com>
2014-07-03 21:41:49 -0400

    added ruby to default.rb

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,11 @@
 name             'Chef-Browncow'
 maintainer       'Stephen Purr'
 maintainer_email 'spurr@articulate.com'
-license          'All rights reserved, preserved and you got served'
+license          'All rights reserved, preserved and you-got-served'
 description      'Configures the browncow server on EC2'
 long_description 'Configuration for Browncow Inc servers'
-version          '0.1.2'
+version          '0.1.4'
 
-%w{ ruby }.each do |cookbook|
+%w{ apt ruby curl apache2 }.each do |cookbook|
   depends cookbook
 end
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,1 +1,4 @@
+include_recipe "apt"
 include_recipe "ruby"
+include_recipe "curl"
+include_recipe "apache2"
