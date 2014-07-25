f6f9c2cda3c38bc87210d3c6ff828b579d0ee293
Steve Purr <spurr@articulate.com>
2014-07-25 19:57:56 -0400

    0.3.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'spurr@articulate.com'
 license          'All rights reserved, preserved and you-got-served'
 description      'Configures the browncow server on EC2'
 long_description 'Configuration for Browncow Inc servers'
-version          '0.3.0'
+version          '0.3.1'
 
 %w{ apt ruby curl apache2 git hostsfile }.each do |cookbook|
   depends cookbook
