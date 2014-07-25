4f68ca3f4e086a9f8db79606c74b407931f6cee8
Steve Purr <spurr@articulate.com>
2014-07-25 18:41:22 -0400

    added hostsfile cookbook to the mix

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,8 @@ maintainer_email 'spurr@articulate.com'
 license          'All rights reserved, preserved and you-got-served'
 description      'Configures the browncow server on EC2'
 long_description 'Configuration for Browncow Inc servers'
-version          '0.2.1'
+version          '0.3.0'
 
-%w{ apt ruby curl apache2 git }.each do |cookbook|
+%w{ apt ruby curl apache2 git hostsfile }.each do |cookbook|
   depends cookbook
 end
